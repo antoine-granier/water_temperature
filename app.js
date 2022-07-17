cc58f60f090360b4fe317847894b3bdc855c7708
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2022-07-17 23:04:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,122 @@
+const body = document.querySelector('body');
+const slides = document.getElementsByClassName('swiper-slide');
+const close_btn = document.querySelector('.close-btn');
+const popup = document.querySelector('.popup');
+const main_popup = document.querySelector('.main-popup');
+const stationId = document.querySelector('.station-id');
+const stationTitle = document.querySelector('.station-title');
+const darkModBtn = document.querySelector('.dark-mod');
+
+const compareTime = (a, b) => {
+    const objA = a.x.split(' ');
+    const objB = b.x.split(' ');
+    if(objA[0] < objB[0]) {
+        return -1;
+    }
+    if(objA[0] > objB[0]) {
+        return 1;
+    }
+    if(objA[0] == objB[0]) {
+        if(objA[1] < objB[1]) {
+            return -1;
+        }
+        if(objA[1] > objB[1]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+const jsonToChartData = (json) => {
+    let data = [];
+    //console.log(json);
+    for(const object in json) {
+        //console.log(json[object]);
+        const station = json[object];
+        data.push({x: station.date_mesure_temp+" "+station.heure_mesure_temp, y: parseFloat(station.resultat.toPrecision(3))});
+    }
+    data.sort(compareTime);
+    return data;
+}
+
+[...slides].forEach(slide => {
+    slide.addEventListener('click', () => {
+        popup.style.display = 'flex';
+        popup.scrollIntoView({behavior: "smooth"});
+	    main_popup.style.cssText = 'animation:slide-in .5s ease; animation-fill-mode: forwards;';
+        const chart = document.querySelector('.chart');
+        chart.style.display = "none";
+        chart.innerHTML = "<canvas id=\"line-chart\"></canvas>"
+        //stationId.innerHTML = slide.id;
+        //console.log(slide.id);
+        $.ajax({
+            url: 'https://hubeau.eaufrance.fr/api/v1/temperature/chronique?code_station=' + slide.id + '&size=100&sort=desc&pretty',
+            beforeSend: () => {
+                $('.wait').show();
+                $('#line-chart').hide();
+            },
+            complete: (data) => {
+                stationTitle.innerHTML = data.responseJSON.data[0].libelle_station;
+                //console.log(data.responseJSON.data);
+                const ctx = document.querySelector('#line-chart').getContext('2d');
+                const config = {
+                    type: 'line',
+                    data: {
+                        labels: [],
+                        datasets: [{
+                            data: jsonToChartData(data.responseJSON.data),
+                            label: "Temperature (°C)",
+                            borderColor: "#3e95cd",
+                            fill: false
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        scales: {
+                            xAxes: [{
+                                type: 'time',
+                                distribution: 'linear',
+                            }],
+                            title: {
+                                display: false,
+                            }
+                        }
+                    }
+                };
+                new Chart(ctx, config);
+                $('.wait').hide();
+                $('.chart').show();
+                $('#line-chart').show();
+            }
+        });
+    })
+});
+
+close_btn.addEventListener('click', () => {
+    const navBar = document.querySelector('.nav-bar');
+    navBar.scrollIntoView({behavior: "smooth"});
+	main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+	setTimeout(() => {
+		popup.style.display = 'none';
+	}, 500);
+});
+
+darkModBtn.addEventListener('click', () => {
+    close_btn.classList.remove('close-btn-dark', 'close-btn-light');
+    if(popup.classList.toggle('active')) {
+        body.style.background = "white";
+        popup.style.background = "white";
+        main_popup.style.background = "white";
+        popup.style.color = "#222";
+        close_btn.style.border = ".6px solid white";
+        close_btn.classList.add('close-btn-light');
+    } else {
+        body.style.background = "#111";
+        popup.style.background = "#222";
+        main_popup.style.background = "#222";
+        popup.style.color = "white";
+        close_btn.style.border = ".6px solid #222";
+        close_btn.classList.add('close-btn-dark');
+    }
+});
---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Water Temperature Gironde</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+    <div class="nav-bar">
+        <div class="title">
+            <ion-icon name="water"></ion-icon>
+            Water Temperature
+        </div>
+        <img class="dark-mod" src="images/day-and-night.png" alt="Dark Mod">
+    </div>
+    <div class="swiper mySwiper">
+        <div class="ocean">
+            <div class="wave"></div>
+            <div class="wave"></div>
+        </div>
+        <div class="swiper-wrapper">
+    <?php
+        $json = file_get_contents("https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement=33&format=json&pretty");
+        if(!$json) {
+            echo "<h2>Une erreur est survenue. Vérifier votre connexion...</h2>";
+        } else {
+            $result = json_decode($json, true);
+            $nb_station = $result["count"];
+            $station_code = array();
+            for($i = 0; $i < $nb_station; $i++) {
+                array_push($station_code, $result["data"][$i]["code_station"]);
+            }
+            foreach($station_code as $code) {
+                $json = file_get_contents("https://hubeau.eaufrance.fr/api/v1/temperature/chronique?code_station=".$code."&size=1&sort=desc&pretty");
+                $result = json_decode($json, true);
+                $data = $result["data"][0];
+                echo "<div class=\"swiper-slide\" id=\"".$code."\"><div class=\"station_card\"><h2 class=\"libelle-station\">".explode(' à ', $data["libelle_station"])[0]."</h2><p class=\"temperature\"><ion-icon name=\"thermometer-outline\"></ion-icon> ".substr((string) $data["resultat"], 0, 2)."°C</p><p class=\"adress\">".$data["libelle_commune"].", ".$data["code_commune"]."</p></div></div>";
+            }
+            //var_dump($station_code);
+        }
+    ?>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+    <div class="popup">
+        <h3 class="station-title"></h3>
+        <ion-icon class="close-btn close-btn-dark" name="close-outline"></ion-icon>
+        <div class="main-popup">
+                <div class="wait"><div></div><div></div><div></div><div></div></div>
+                <div class="chart"></div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="app.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script>
+        if(screen.whidth < 1200 && screen.width > 894) {
+            var swiper = new Swiper(".mySwiper", {
+                slidesPerView: 3,
+                spaceBetween: 50,
+                centeredSlides: true,
+                cssMode: true,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                mousewheel: true,
+                keyboard: true,
+            });
+        } else if(screen.width > 1200) {
+            var swiper = new Swiper(".mySwiper", {
+                slidesPerView: 4,
+                spaceBetween: 50,
+                centeredSlides: true,
+                cssMode: true,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                mousewheel: true,
+                keyboard: true,
+            });
+        } else if(screen.width > 600) {
+            var swiper = new Swiper(".mySwiper", {
+                slidesPerView: 2,
+                spaceBetween: 50,
+                centeredSlides: true,
+                cssMode: true,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                mousewheel: true,
+                keyboard: true,
+            });
+        } else {
+            var swiper = new Swiper(".mySwiper", {
+                slidesPerView: 1.5,
+                spaceBetween: 50,
+                centeredSlides: true,
+                cssMode: true,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+                mousewheel: true,
+                keyboard: true,
+            });
+        }
+    </script>
+</body>
+</html>
